27ffd77e00f8c718f1102f80fc37c1d005d8f1cc
bilal2006 <46657984+bilal2006@users.noreply.github.com>
2021-03-18 15:22:30 -0400

    Update GearHoundsTester.java

--- GearHoundsTester.java
+++ GearHoundsTester.java
@@ -13,19 +13,33 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class GearHoundsTestingCodeAuto extends LinearOpMode {
     private DcMotor leftFront = null;
     private DcMotor leftBack = null;
-    //private DcMotor rightFront = null;
-    //private DcMotor rightBack = null;
+    private DcMotor rightFront = null;
+    private DcMotor rightBack = null;
 
     @Override
     public void runOpMode() throws InterruptedException {
         leftFront = hardwareMap.dcMotor.get("leftFront");
         leftBack = hardwareMap.dcMotor.get("leftBack");
-        //rightFront = hardwareMap.dcMotor.get("rightFront");
-        //rightBack = hardwareMap.dcMotor.get("rightBack");
+        rightFront = hardwareMap.dcMotor.get("rightFront");
+        rightBack = hardwareMap.dcMotor.get("rightBack");
         leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
         leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        
         leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        
         leftBack.setTargetPosition(293);
+        leftFront.setTargetPosition(293);
+        rightBack.setTargetPosition(293);
+        rightFront.setTargetPosition(293);
+        
+        leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        
     }
+
 }
