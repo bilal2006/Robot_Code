6bcd2551caf85ea3b4ef26c283529db971d1993e
bilal2006 <46657984+bilal2006@users.noreply.github.com>
2021-03-18 15:46:49 -0400

    Update GearHoundsTester.java

--- GearHoundsTester.java
+++ GearHoundsTester.java
@@ -24,16 +24,16 @@ public class GearHoundsTestingCodeAuto extends LinearOpMode {
         rightBack = hardwareMap.dcMotor.get("rightBack");
         leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
         leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
-
+        
         leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        leftBack.setTargetPosition(100000);
-        leftFront.setTargetPosition(100000);
-        rightBack.setTargetPosition(100000);
-        rightFront.setTargetPosition(100000);
+        leftBack.setTargetPosition(293);
+        leftFront.setTargetPosition(293);
+        rightBack.setTargetPosition(293);
+        rightFront.setTargetPosition(2933);
 
         waitForStart();
 
@@ -41,8 +41,18 @@ public class GearHoundsTestingCodeAuto extends LinearOpMode {
         leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-
+        
+        while(leftFront.getCurrentPosition()<leftFront.getTargetPosition()){
+            leftFront.setPower(0.75);
+            leftBack.setPower(0.75);
+            rightFront.setPower(0.75);
+            rightBack.setPower(0.75);
+        }
+        leftBack.setPower(0.0);
+        leftFront.setPower(0.0);
+        rightBack.setPower(0.0);
+        rightFront.setPower(0.0);
+                
     }
 
 }
